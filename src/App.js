d81090a79995b2caf80663e8de82f57c4fb3a9ae
Arun <arunkumar.venkatesan@arcadix.com>
2022-11-12 23:43:19 +0530

    storage added

--- src/App.js
+++ src/App.js
@@ -89,9 +89,16 @@ const App = ({ signOut }) => {
                         variation="quiet"
                         required
                     />
+                    <View
+                        name="image"
+                        as="input"
+                        type="file"
+                        style={{ alignSelf: "end" }}
+                    />
                     <Button type="submit" variation="primary">
                         Create Note
                     </Button>
+                   
                 </Flex>
             </View>
             <Heading level={2}>Current Notes</Heading>
@@ -121,12 +128,7 @@ const App = ({ signOut }) => {
                 ))}
             </View>
             <Button onClick={signOut}>Sign Out</Button>
-            <View
-                name="image"
-                as="input"
-                type="file"
-                style={{ alignSelf: "end" }}
-            />
+            
         </View>
     );
 };
